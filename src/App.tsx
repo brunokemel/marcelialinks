2dfb5997aa9229136c9cd4af63188a07ce13c335
Bruno Kemel <br.kemel@gmail.com>
2025-05-26 18:03:38 -0300

    foto atualizada

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { GlobalStyle } from './styles/global';
 const profileData = {
   name: "Marcelia Corrêa",
   bio: "Especialista em terapias naturais e energéticas.",
-  imageUrl: "https://scontent.fbel10-1.fna.fbcdn.net/v/t39.30808-6/438171004_7104316853005508_6970590181989255742_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeE0cNVVoIrXEgatYnG8OFTBsHm1MuEvtIawebUy4S-0huEicBzwJbZB-6CNJBszKGGTAmxebttANqqkPNPP6ns3&_nc_ohc=gbNakP-pQ4oQ7kNvwG-ycD_&_nc_oc=AdnCTWyBt0hSJIM5Il-ykJouPuvhhHUjoDpkMDGvsyEJsXFlxMZ4AITto-XufPFdb_M&_nc_zt=23&_nc_ht=scontent.fbel10-1.fna&_nc_gid=yZdjvvMq4vSNxvA3ZJPxaA&oh=00_AfLpkBp861AiSaau4cnsNLFReCZOKkpS7QIEKuWb3eO77A&oe=6822C03B",
+  imageUrl: "https://scontent.fbel10-1.fna.fbcdn.net/v/t39.30808-6/438171004_7104316853005508_6970590181989255742_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=uxJfbix6jbEQ7kNvwGJYLPZ&_nc_oc=AdkWft7RTW8iMXrhEmCYXuw04il-TowZfJmUFwGyvaWzNOUe912SYf1pYXVzZCYVMJM&_nc_zt=23&_nc_ht=scontent.fbel10-1.fna&_nc_gid=Ig601f_LMQPGzxwslRUkxg&oh=00_AfK1opN4qiWojFWmDdjDESYAM2CTzENelEPbZySGZsDFQQ&oe=683AB37B",
   links: [
     {
       title: "Instagram",
