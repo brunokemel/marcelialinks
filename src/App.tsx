58e4bbeacd88f641ecf01801e78871f4bcb17df9
Bruno Kemel <br.kemel@gmail.com>
2025-06-22 16:31:43 -0300

    img local + logo URL

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Profile } from './components/Profile';
 import { Footer } from './components/Footer';
 import { GlobalStyle } from './styles/global';
+import marceImg from '../src/assets/marcelia2.png';
 
 const profileData = {
   name: "Marcelia Corrêa",
   bio: "Especialista em terapias naturais e energéticas.",
-  imageUrl: "https://scontent.fbel10-1.fna.fbcdn.net/v/t39.30808-1/438171004_7104316853005508_6970590181989255742_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=110&ccb=1-7&_nc_sid=e99d92&_nc_eui2=AeE0cNVVoIrXEgatYnG8OFTBsHm1MuEvtIawebUy4S-0huEicBzwJbZB-6CNJBszKGGTAmxebttANqqkPNPP6ns3&_nc_ohc=5KPEBO8BCeYQ7kNvwF-bdZu&_nc_oc=Adn85HAYxpSF5BhI7GhuqHYgZPsJoXxsJeWuc811rJY1r86B8Wfr7MPR4yf4m6Gs9E0&_nc_zt=24&_nc_ht=scontent.fbel10-1.fna&_nc_gid=A7csrBvXf7gccqdqIXEBmw&oh=00_AfOkVVJE0iKqIEU3Gpqub1mBEa7Q1qF1kEb4p1QQ09Ecqg&oe=685CDB79",
+  imageUrl: marceImg,
   links: [
     {
       title: "Instagram",
